2940381fb1e590e42dc9a34fc4814843da154b0f
Christine <calvertc11@gmail.com>
2019-09-19 19:13:25 -0400

    Added program

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ISMMethods
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+
+                try
+                {
+                    Console.WriteLine("Please input your name");
+                    string Name = HelloName();
+
+                Console.WriteLine("Hello " + Name + "!");
+
+                }
+
+                catch
+                {
+                    Console.WriteLine("There was an error with this code");
+                }
+            }
+
+        public static string HelloName()
+        {
+            string UserName = Console.ReadLine();
+
+            return UserName;
+        }
+    }
+    
+}
